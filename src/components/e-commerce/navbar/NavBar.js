f4940898233173bdf09792839263666c6d3682b3
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-04-22 13:02:29 +0530

    Update NavBar.js

--- src/components/e-commerce/navbar/NavBar.js
+++ src/components/e-commerce/navbar/NavBar.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useSelector } from 'react-redux';
 import './NavBar.css';
 
 function NavBar() {
+    const cart = useSelector(state => state.cartReducer.cart);
+
+    let count = 0;
+    cart.forEach(item => count += item.quantity);
+
     return (
         <nav>
-            <h2 className='banner'>My Myntra</h2>
+            <h1 className='banner'>My Store</h1>
             <div className="right-layout">
                 <div className="cart-layout">
                     <AiOutlineShoppingCart />
-                    <h3>4</h3>
+                    <h3>{count}</h3>
                 </div>
             </div>
         </nav>
