cd9798941c83de618004580919310b0c8004236b
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-04-22 13:23:24 +0530

    Update SingleProduct.js

--- src/components/e-commerce/singleProduct/SingleProduct.js
+++ src/components/e-commerce/singleProduct/SingleProduct.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import './SingleProduct.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart } from '../../../redux/slices/cartSlice';
 
 function SingleProduct({ product }) {
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector(state => state.cartReducer.cart);
+  const curItem = cart.find(item => item.id === product.id);
+  const curQuantity = curItem ? curItem.quantity : 0;
+
   return (
     <div className='SingleProduct'>
-      <img className='productImg' src={product.images[0]} alt={product.title} />
+      <div className='imgDiv'><img className='productImg' src={product.images[0]} alt={product.title} /></div>
       <div className="productInfo">
         <h2 className='productTitle'>{product.title}</h2>
         <p className='productPrice'>{product.price}</p>
+        <p>Category: {product.category.name}</p>
       </div>
       <div className='cartInfo'>
-        <button className='button'>-</button>
-        <h4>0</h4>
-        <button className='button'>+</button>
+        <button className='button' onClick={() => { dispatch(removeFromCart(product.id)) }}>-</button>
+        <h2>{curQuantity}</h2>
+        <button className='button' onClick={() => { dispatch(addToCart(product.id)) }}>+</button>
       </div>
     </div>
   )
