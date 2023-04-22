80249cfcd68d95244166e4bfeafb73541a3a6395
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-04-22 13:21:43 +0530

    Update ProductList.js

--- src/components/e-commerce/productList/ProductList.js
+++ src/components/e-commerce/productList/ProductList.js
@@ -1,31 +1,43 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { loadProducts } from '../../../redux/slices/ProductSlice';
+import { fetchData } from '../../../redux/slices/ProductSlice';
 import SingleProduct from '../singleProduct/SingleProduct';
 import './ProductList.css';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
 
 function ProductList() {
 
-  const dispatach = useDispatch();
+  const dispatch = useDispatch();
   const products = useSelector(state => state.productReducer.products)
+  const status = useSelector(state => state.productReducer.status);
+  const error = useSelector(state => state.productReducer.error)
 
   useEffect(() => {
-    fetchData();
+    dispatch(fetchData())
   }, [])
 
-  async function fetchData() {
-    // const response = await fetch('https://fakestoreapi.com/products')
-    const response = await fetch('https://api.escuelajs.co/api/v1/products')
-    const data = await response.json();
-    dispatach(loadProducts(data));
+  if (status === 'loading') {
+    const antIcon = <LoadingOutlined style={{ fontSize: '60px', position: 'absolute', top: '40%', left: '50%' }} spin />;
+    return <Spin indicator={antIcon} />;
   }
 
+  if (status === 'failed') {
+    return (
+      <>
+        <h2>Uh oh! Something went wrong.</h2>
+        <h3>{error}</h3>
+      </>
+    );
+  }
 
   return (
     <div className='productList'>
-      {products.map(item => <SingleProduct product={item} />)}
+      {products.map(item =>
+        <SingleProduct key={item.id} product={item} />
+      )}
     </div>
   )
 }
 
-export default ProductList
+export default ProductList;
